a275e83b5972604793f9814913a6c3bbc8227b51
Pawankumar2025 <pawankumarbattula2000@gmail.com>
2025-04-26 00:13:02 +0530

    Update db.php

--- config/db.php
+++ config/db.php
@@ -1,6 +1,6 @@
 <?php
 $host = 'localhost';       
-$db   = 'test';              
+$db   = 'expenses_db';              
 $user = 'root';        
 $pass = '';             
 $charset = 'utf8mb4';      
